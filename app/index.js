5f8518c43d048af930adefcd327aecbbe87bd14d
Nour Alhashim <alhashimnourr@gmail.com>
2025-06-27 12:18:42 +0300

    Edit index.js and make it redirect to index.html

---
+++ app/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path'); // ✅ Don't forget this!
+const app = express();
+
+// Choose the port from the environment or use 3000 for local dev
+const PORT = process.env.PORT || 5000;
+
+// A simple home route
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+  // res.send('🚀 Hello from your Node.js app!');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
+});
